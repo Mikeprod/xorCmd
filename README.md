5c540a628dd3b65c0ac964eb0f901481ebf02de0
Mikeprod <mikeprod.tm@gmail.com>
2015-08-17 22:59:27 +0200

    Adding VS project
    TODO :
    - comment each part
    - better programming

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # xorCmd
+xorCmd stand for xor file in command line. This is a C# program.
+##Documentation
+Simply type : 
+	xor.exe pathOfTheFileToXor latin9XorKey pathOfTheResultingFile
+###Warnings
+* The `latin9XorKey` must be smaller than the size of the file to xor.
+* Beware the resulting file if exists will be overwrited !
---
+++ XorCmd/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace XorCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                if (File.Exists(args[0]))
+                    xorChar(args[0], Encoding.GetEncoding("iso-8859-15").GetBytes(args[1].ToCharArray()), new FileStream(args[2], FileMode.OpenOrCreate));
+                else
+                    Console.WriteLine("The file to xor does not exists !");
+            }
+            else
+            {
+                if(args.Length==2 || args.Length>3)
+                    Console.WriteLine("Not the right amount of arguments.");
+                if (args.Length == 0|| args[0].Equals("-help")||args[0].Equals("--help")||args[0].Equals("/help") || args[0].Equals("-h") || args[0].Equals("/h"))
+                {
+                    Console.WriteLine("Syntax : xor.exe fileToXor xorKey fileXored");
+                    Console.WriteLine("Beware the fileXored argument will create or overwrite the file.");
+                }
+            }
+                
+        }
+
+        static void xorChar(string file, byte[] xorKey, FileStream xFile)
+        {
+            byte xoredByte;
+            int i = 0;
+
+            try
+            {
+                byte[] readBuffer = File.ReadAllBytes(file);
+                if (xorKey.Length > readBuffer.Length)
+                {
+                    Console.WriteLine("The Key cannot be bigger than the data to xor!!");
+                    return;
+                }
+                foreach (byte b in readBuffer)
+                {
+                    xoredByte = (byte)(b ^ xorKey[i++]);
+                    xFile.WriteByte(xoredByte);
+                    if (i == xorKey.Length)
+                        i = 0;
+                }
+
+                xFile.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
