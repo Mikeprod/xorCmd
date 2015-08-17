da57d59c8236404b0585e65345c9d2ef29301309
Mikeprod <mikeprod.tm@gmail.com>
2015-08-17 23:14:53 +0200

    Added comments

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # xorCmd
 xorCmd stand for xor file in command line. This is a C# program.
 ##Documentation
-Simply type : 
-	xor.exe pathOfTheFileToXor latin9XorKey pathOfTheResultingFile
+Simply type : `xor.exe pathOfTheFileToXor latin9XorKey pathOfTheResultingFile`
 ###Warnings
 * The `latin9XorKey` must be smaller than the size of the file to xor.
 * Beware the resulting file if exists will be overwrited !
--- XorCmd/Program.cs
+++ XorCmd/Program.cs
@@ -6,12 +6,16 @@ namespace XorCmd
 {
     class Program
     {
+        /// <summary>
+        /// Program entry point
+        /// </summary>
+        /// <param name="args">Arguments within the command line</param>
         static void Main(string[] args)
         {
-            if (args.Length == 3)
+            if (args.Length == 3) //Authorising only 3 arguments
             {
-                if (File.Exists(args[0]))
-                    xorChar(args[0], Encoding.GetEncoding("iso-8859-15").GetBytes(args[1].ToCharArray()), new FileStream(args[2], FileMode.OpenOrCreate));
+                if (File.Exists(args[0])) //Verifying if the file from the path given really exists
+                    xorChar(args[0], Encoding.GetEncoding("iso-8859-15").GetBytes(args[1].ToCharArray()), new FileStream(args[2], FileMode.OpenOrCreate)); //XorKey is for now only Latin 9 encoded
                 else
                     Console.WriteLine("The file to xor does not exists !");
             }
@@ -19,7 +23,7 @@ namespace XorCmd
             {
                 if(args.Length==2 || args.Length>3)
                     Console.WriteLine("Not the right amount of arguments.");
-                if (args.Length == 0|| args[0].Equals("-help")||args[0].Equals("--help")||args[0].Equals("/help") || args[0].Equals("-h") || args[0].Equals("/h"))
+                if (args.Length == 0|| args[0].Equals("-help")||args[0].Equals("--help")||args[0].Equals("/help") || args[0].Equals("-h") || args[0].Equals("/h")) //Deflaut an help messages
                 {
                     Console.WriteLine("Syntax : xor.exe fileToXor xorKey fileXored");
                     Console.WriteLine("Beware the fileXored argument will create or overwrite the file.");
@@ -27,7 +31,12 @@ namespace XorCmd
             }
                 
         }
-
+        /// <summary>
+        /// Xoring function
+        /// </summary>
+        /// <param name="file">Filepath to the file getting xored</param>
+        /// <param name="xorKey">XorKey to xor the file with</param>
+        /// <param name="xFile">Resulting file (xored)</param>
         static void xorChar(string file, byte[] xorKey, FileStream xFile)
         {
             byte xoredByte;
@@ -38,10 +47,10 @@ namespace XorCmd
                 byte[] readBuffer = File.ReadAllBytes(file);
                 if (xorKey.Length > readBuffer.Length)
                 {
-                    Console.WriteLine("The Key cannot be bigger than the data to xor!!");
+                    Console.WriteLine("The Key cannot be bigger than the data to xor!!"); //If the user regardless the warning inputs a key longer than the file.
                     return;
                 }
-                foreach (byte b in readBuffer)
+                foreach (byte b in readBuffer) //Xoring the file byte to byte with the key depending on the key length
                 {
                     xoredByte = (byte)(b ^ xorKey[i++]);
                     xFile.WriteByte(xoredByte);
